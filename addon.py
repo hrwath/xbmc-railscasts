9eb6094675526bb6d958a0f6de3e3e01fdebcea5
Paweł Lenart <pawel.lenart@gmail.com>
2012-10-21 21:53:04 +0200

    additional fixes

--- addon.py
+++ addon.py
@@ -1,25 +1,28 @@
-#    Railscasts XBMC Addon
-#    -------------------
-#
-#    Watch screencasts from http://railscasts.com in XBMC.
-#
-#    :copyright: (c) 2012 by Jonathan Beluch
-#    :license: GPLv3, see LICENSE.txt for more details.
+'''
+    Railscasts XBMC Addon
+    -------------------
+
+    Watch screencasts from http://railscasts.com in XBMC.
+
+    :copyright: (c) 2012 by Jonathan Beluch
+    :license: GPLv3, see LICENSE.txt for more details.
+'''
 
 from urllib2 import urlopen
 from xml.dom import minidom
+from xbmcswift2 import Plugin
 
 PLUGIN_NAME = 'Railscasts'
 PLUGIN_ID = 'plugin.video.railscasts'
 plugin = Plugin(PLUGIN_NAME, PLUGIN_ID, __file__)
 
-rss_feed = "http://feeds.feedburner.com/railscasts"
-
 def get_rss():
-    rss = urlopen(rss_feed)
+    '''Gets RSS feed for the episodes'''
+    rss = urlopen('http://feeds.feedburner.com/railscasts')
     return rss
 
 def get_episodes():
+    '''Scrapes the episode data from XML'''
     rss = get_rss()
 
     tree = minidom.parse(rss)
@@ -27,34 +30,35 @@ def get_episodes():
 
     episodes = []
 
-    _titles = nodes.getElementsByTagName('title')
+    _titles = nodes[0].getElementsByTagName('title')
     _titles.pop(0)
-    _descriptions = nodes.getElementByTagName('description')
+    _descriptions = nodes[0].getElementsByTagName('description')
     _descriptions.pop(0)
-    _durations = nodes.getElementsByTagName('itunes:duration')
-    _urls = nodes.getElementsByTagName('enclosure')
+    _durations = nodes[0].getElementsByTagName('itunes:duration')
+    _urls = nodes[0].getElementsByTagName('enclosure')
 
-    for i in range(0, _titles.length-1):
-        episodes[i] = {
+    for i in range(0, _titles.length):
+        print i
+        episodes.append({
             'title': _titles[i].childNodes[0].toxml() + ' (' + _durations[i].childNodes[0].toxml() + ')',
             'description': _descriptions[i].childNodes[0].toxml(),
             'url': _urls[i].getAttribute('url')
-        }
+        })
 
     return episodes
 
 @plugin.route('/')
 def index():
-    items = [
-        {
-            'label': episode['title'],
-            'path': episode['url'],
-            'info': {
-                'plot': episode['description']
-            },
-            'is_playable': True,
-        } for episode in get_episodes()
-    ]
+    '''The main menu for add-on'''
+    items = [{
+        'label': episode['title'],
+        'path': episode['url'],
+        'thumbnail': '',
+        'info': {
+            'plot': episode['description']
+        },
+        'is_playable': True,
+    } for episode in get_episodes()]
 
     return items
 
